a4364280bc32979041d8e257574c55fbf3f408f9
mattscoo <25484517+Mattscoo@users.noreply.github.com>
2018-05-06 12:56:04 -0400

    random cleanup

--- src/main/java/edu/brown/cs/mldm/main/Main.java
+++ src/main/java/edu/brown/cs/mldm/main/Main.java
@@ -19,7 +19,6 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-
 		OptionParser parser = new OptionParser();
 		parser.accepts("gui");
 		parser.accepts("port").withRequiredArg().ofType(Integer.class).defaultsTo(DEFAULT_PORT);
